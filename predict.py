c9ae07483ec0cd2509689dc4f3c109fa92dc5dab
HuangYanh <83455047+huangyanh@users.noreply.github.com>
2024-05-16 15:29:43 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,9 +5,9 @@ import torch
 from ultralytics import YOLO
 if __name__ == '__main__':
 
-    # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # select your model.pt path  server_last.pt C:\\zuomian\\Yolo\\ultralytics-FL\\runs\\train\\yolov5_2\\weights\\best.pt
+    # model = YOLO('yolov8n.yaml').load('yolov8n.pt') 
     model = YOLO('ultralytics/cfg/models/v8/yolov8n.yaml')
-    a = torch.load('G:\\zuomian\\FL\\server_last\\C3\\v8_5.pt')     #runs/train/FLv5_23/weights/last.pt server_last.pt
+    a = torch.load('G:\\zuomian\\FL\\server_last\\C3\\v8_5.pt')    
     model.model.load(a)
     model.model.names = ['vessel']
     model.predict(source='dataset/Input2/client3',
